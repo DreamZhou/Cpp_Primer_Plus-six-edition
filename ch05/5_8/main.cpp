432e06a9c9cca15e9464f2a691a90ec3175f642b
DreamZhou <qj198908@126.com>
2017-06-01 21:39:33 +0800

    noting!

--- ch05/5_8/main.cpp
+++ ch05/5_8/main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
-
-using namespace std;
-
-int main(int argc, char *argv[])
+#include<cstring>
+const int ArraySize = 30 ;
+int main()
 {
-    cout << "Hello World!" << endl;
-    return 0;
+  char words[ArraySize];
+  int count =0;
+  std::cout << "Enter words (to stop, type the word done): \n";
+  while (std::cin >> words) {
+      if(strcmp(words,"done")==0){
+          break;
+      }
+      ++count;
+  }
+  std::cout << "You entered a total of " << count << " words." <<std::endl;
+
+  return 0;
 }
