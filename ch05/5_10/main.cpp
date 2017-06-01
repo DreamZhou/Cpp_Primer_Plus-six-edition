1fdfe662ce71ff511c10cb96240cb81ef74aec38
DreamZhou <qj198908@126.com>
2017-06-01 22:44:03 +0800

    0

--- ch05/5_10/main.cpp
+++ ch05/5_10/main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
 
-using namespace std;
 
-int main(int argc, char *argv[])
+int main()
 {
-    cout << "Hello World!" << endl;
+    int count;
+
+    std::cout << "Enter numbers of rows: " ;
+    std::cin >> count ;
+    for (int i = 0; i < count; ++i){
+        for (int j = 0; j < count; ++j){
+             if(j < count - i - 1)
+                 std::cout << ".";
+              else
+                 std::cout << "*";
+        }
+        std::cout <<  std::endl;
+
+    }
     return 0;
 }
