8f523749b7eb2ef3105848dcd4eace737b4d8ee0
DreamZhou <qj198908@126.com>
2017-06-01 21:52:44 +0800

    .;l

--- ch05/5_9/main.cpp
+++ ch05/5_9/main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
+#include<string>
 
-using namespace std;
-
-int main(int argc, char *argv[])
+int main()
 {
-    cout << "Hello World!" << endl;
+    std::string word ;
+    int count = 0 ;
+
+    std::cout << "Enter words (to stop, type the word done): \n";
+    while (std::cin >> word) {
+        if (word == "done"){
+            break;
+        }
+        ++count;
+    }
+    std::cout << "You enter a total of " << count << " words." << std::endl;
     return 0;
 }
