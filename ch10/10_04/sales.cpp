093faad7ef1fda9d9923444ce5acf884157a4b4a
DreamZhou <qj198908@126.com>
2017-06-05 21:12:59 +0800

    Edit sales.cpp

--- ch10/10_04/sales.cpp
+++ ch10/10_04/sales.cpp
@@ -5,13 +5,14 @@
 SALES::Sales::Sales(std::array<double, SALES::QUARTERS> sa)
 {
     saledate = sa;
-    double total = 0.0;
+//    double total = 0.0;
+//    for (auto i : sa){
+//        total += i;
+//        std::cout << i << " ";
+//    }
+//   average = total / QUARTERS;
+    average = std::accumulate(sa.begin(),sa.end(),0.0)/sa.size();
     std::array<double,SALES::QUARTERS>::iterator itor;
-    for (auto i : sa){
-        total += i;
-        std::cout << i << " ";
-    }
-    average = total / QUARTERS;
     itor = std::min_element(sa.begin(),sa.end());
     min = *itor;
     itor = std::max_element(sa.begin(),sa.end());
@@ -34,9 +35,13 @@ void SALES::Sales::showSales() const
 
 SALES::Sales SALES::setSales()
 {   std::array<double,QUARTERS> sa;
-    for (int i = 0; i < QUARTERS; i++){
-        std::cout << "Enter the sales #" << i+1 << " :";
-        std::cin >> sa[i];
+//    for (int i = 0; i < QUARTERS; i++){
+//        std::cout << "Enter the sales #" << i+1 << " :";
+//        std::cin >> sa[i];
+//    }
+    for (auto &i :sa){
+        std::cout << "Enter the sales : ";
+        std::cin >> i;
     }
 
     return SALES::Sales(sa) ;
